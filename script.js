fae153a84d64275c7fc33c086f3b8ac4f3ff42c8
Sebastian Iannotta <sebaiannotta20@gmail.com>
2025-01-06 16:29:20 -0300

    Elimino archivos

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.getElementById("menu-button");
-const menu = document.getElementById("menu");
-
-// Alternar el estado del menú al hacer clic
-menuButton.addEventListener("click", () => {
-    menuButton.classList.toggle("open");
-    menu.classList.toggle("open");
-});
